24090be34dc406d2c0e5b799a3a56b6f66e27eb4
CaioCTerra <caio.terra@gsuite.iff.edu.br>
2026-08-21 21:06:51 -0300

    Começo estrutura Tema
    Adicionado classes, porem sem estrutura logica em si, logo mais estarei acrescentando mais coisas

--- src/br/edu/iff/dominio/tema/Tema.java
+++ src/br/edu/iff/dominio/tema/Tema.java
@@ -1,4 +1,29 @@
 package br.edu.iff.dominio.tema;
 
 public class Tema {
+    private String nome;
+
+    public criar (long id, String nome){
+        this.setNome(nome);
+    }
+
+
+    public reconstituir(long id, String nome){
+        return null;
+
+    
+    }
+
+    public Tema(long id, String nome) {
+        this.nome = nome;
+        this.id= id;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null || nome.trim().isEmpty()){
+            throw new IllegalArgumentException("Nome do nosso tema não pode ser nulo ou vazio");
+        } else {
+            this.nome = nome;
+        }
+    }
 }
