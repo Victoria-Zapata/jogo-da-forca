8731298ecc4586f3a8542cfbcca25fbe02bd66ea
Victoria-Zapata <victoriazapata.gz@gmail.com>
2026-09-05 20:16:27 -0300

    Atualização: Implementação do Factory de Jogador

--- src/br/edu/iff/jogoforca/dominio/jogador/JogadorFactoryImpl.java
+++ src/br/edu/iff/jogoforca/dominio/jogador/JogadorFactoryImpl.java
@@ -1,5 +1,34 @@
 package br.edu.iff.jogoforca.dominio.jogador;
 
-public class JogadorFactoryImpl {
+import br.edu.iff.factory.EntityFactory;
 
+public class JogadorFactoryImpl extends EntityFactory implements JogadorFactory{
+	private static JogadorFactoryImpl soleInstance;
+	
+	public static void createSoleInstance(JogadorRepository repository) {
+		if(soleInstance == null) {
+			soleInstance = new JogadorFactoryImpl(repository);
+		}
+	}
+	
+	public static JogadorFactoryImpl getSoleInstance() {
+		return soleInstance;
+	}
+	
+	private JogadorFactoryImpl(JogadorRepository repository) {
+		super(repository);
+	}
+	
+	private JogadorRepository getJogadorRepository() {
+		return (JogadorRepository) super.getRepository();
+	}
+	
+	public Jogador getJogador(String nome) {
+		if(getJogadorRepository().getPorNome(nome) != null) {
+			throw new IllegalArgumentException("Erro: Já existe um Jogador com esse nome!");
+		}
+		return Jogador.criar(this.getProximoId(), nome);
+	}
+	
 }
+
