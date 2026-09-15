c756a9fe45aea89a42e2a84c023fc53a19d04c73
lucaschalita <lucaschalita@outlook.com>
2026-09-15 00:15:07 -0300

    Implementação da classe PalavraAppService

--- src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
+++ src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
@@ -1,4 +1,49 @@
 package br.edu.iff.bancodepalavras.dominio.palavra;
 
+import br.edu.iff.bancodepalavras.dominio.tema.Tema;
+import br.edu.iff.bancodepalavras.dominio.tema.TemaRepository;
+import br.edu.iff.repository.RepositoryException;
+
 public class PalavraAppService {
+    private static PalavraAppService soleInstance;
+
+    private TemaRepository temaRepository;
+    private PalavraRepository palavraRepository;
+    private PalavraFactory palavraFactory;
+
+    public static void createSoleInstance(TemaRepository temaRepository, PalavraRepository palavraRepository, PalavraFactory palavraFactory) {
+        if (soleInstance == null) {
+            soleInstance = new PalavraAppService(temaRepository, palavraRepository, palavraFactory);
+        }
+    }
+
+    public static PalavraAppService getSoleInstance() {
+        return soleInstance;
+    }
+
+    private PalavraAppService(TemaRepository temaRepository, PalavraRepository palavraRepository, PalavraFactory palavraFactory) {
+        this.temaRepository = temaRepository;
+        this.palavraRepository = palavraRepository;
+        this.palavraFactory = palavraFactory;
+    }
+
+    public boolean novaPalavra(String palavra, long idTema) {
+        if (palavraRepository.getPalavra(palavra) != null) {
+            return true;
+        }
+
+        Tema tema = temaRepository.getPorId(idTema);
+        if (tema == null) {
+            return false;
+        }
+
+        Palavra novaPalavra = palavraFactory.getPalavra(palavra, tema);
+
+        try {
+            palavraRepository.inserir(novaPalavra);
+            return true;
+        } catch (RepositoryException e) {
+            return false;
+        }
+    }
 }
